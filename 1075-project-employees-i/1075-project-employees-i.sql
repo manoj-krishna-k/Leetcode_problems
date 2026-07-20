7aea045317a66652da879017748ad06c5b5484ea
Manoj Krishna K <manojprof6@gmail.com>
2026-07-20 18:19:03 +0530

    Time: 544 ms (50.3%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,2 @@
+/* Write your PL/SQL query statement below */
+select project_id,round(avg(experience_years),2) as average_years from Project join Employee using(employee_id) group by project_id order by project_id;
