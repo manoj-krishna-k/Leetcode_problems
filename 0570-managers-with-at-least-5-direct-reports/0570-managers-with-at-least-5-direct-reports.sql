941282d86ba84c28a17bf4307125e9f56c111fa6
Manoj Krishna K <manojprof6@gmail.com>
2026-07-21 18:42:06 +0530

    Time: 478 ms (30.98%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,4 @@
+/* Write your PL/SQL query statement below */
+select name from Employee where id in (
+    select managerId from (select managerId,count(*) as cnt from Employee group by managerId) where cnt>=5
+)
