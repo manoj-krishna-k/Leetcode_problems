0aa73dd324623dda5382c2a5fe7c6de2e2f3cc2c
Manoj Krishna K <manojprof6@gmail.com>
2026-08-01 14:20:12 +0530

    Time: 66 ms (23.33%), Space: 42.8 MB (41.06%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,12 @@
+class Solution {
+    
+    public boolean predictTheWinner(int[] nums) {
+        return solve(nums,0,nums.length-1)>=0;
+    }
+    public int solve(int[] nums,int i,int j){
+        if(i==j)return nums[i];
+        int left=nums[i]-solve(nums,i+1,j);
+        int rigth=nums[j]-solve(nums,i,j-1);
+        return Math.max(left,rigth);
+    }
+}
