ee37536d1e6e518090504f71e367dc90d9653795
Manoj Krishna K <manojprof6@gmail.com>
2026-08-28 15:56:05 +0530

    Time: 636 ms (11.21%), Space: 42.9 MB (93.93%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return solve(nums,0,0,target);
+    }
+    public int solve(int [] nums,int i,int sum,int target){
+
+        if(i==nums.length)return sum==target?1:0;
+        int add=solve(nums,i+1,sum+nums[i],target);
+        int sub=solve(nums,i+1,sum-nums[i],target);
+        return add+sub;
+
+    }
+}
