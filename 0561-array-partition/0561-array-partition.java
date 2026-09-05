582a3959776578978f8d1eecd31242c8627bfb79
Manoj Krishna K <manojprof6@gmail.com>
2026-09-05 18:26:29 +0530

    Time: 16 ms (97.88%), Space: 49.6 MB (54.14%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -4,7 +4,7 @@ class Solution {
         int n=nums.length;
         int sum=0;
         for(int i=0;i<n;i+=2){
-            sum+=Math.min(nums[i],nums[i+1]);
+            sum+=nums[i];
         }
         return sum;
     }
